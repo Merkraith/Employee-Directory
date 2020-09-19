1673c403433c0bb11d0eec5d25b70d927be1fdce
Mark Heil <mark.heil1122@gmail.com>
2020-09-19 13:43:05 -0700

    implemented jumbotron from bootstrap react into app.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import Container from 'react-bootstrap/Container'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Jumbotron fluid>
+        <Container>
+          <h1>Emloyee Directory</h1>
+          <h2>
+            Search for Employees in the directory
+          </h2>
+        </Container>
+      </Jumbotron>
     </div>
   );
 }
