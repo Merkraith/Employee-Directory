2630118a9c5f34f20c49cfcd5bb6c456f72c9fce
Mark Heil <mark.heil1122@gmail.com>
2020-09-19 20:54:38 -0700

    updated api.js

--- my-app/src/utils/api.js
+++ my-app/src/utils/api.js
@@ -2,17 +2,17 @@ import axios from "axios";
 
 export default {
     searchEmployees: function() {
-      return axios.get("https://randomuser.me/api/?pretty?page=3&results=10&seed=abc")
+      return axios.get("https://my-json-server.typicode.com/merkraith/AvengersRoster/db")
     },
     searchForm: function() {
-      return axios.get("https://randomuser.me/api/?pretty?page=3&results=10&seed=abc")
+      return axios.get("https://my-json-server.typicode.com/merkraith/AvengersRoster/db")
     },
     getEmployeeByName: function() {
-      return axios.get("https://randomuser.me/api/?pretty?page=3&results=10&seed=abc");
+      return axios.get("https://my-json-server.typicode.com/merkraith/AvengersRoster/db");
     },
     // Need to put in correct address to search by employee id
     getEmployeeById: function() {
-      return axios.get("https://randomuser.me/api/?pretty?page=3&results=10&seed=abc");
+      return axios.get("https://my-json-server.typicode.com/merkraith/AvengersRoster/db");
     }
   
   };
